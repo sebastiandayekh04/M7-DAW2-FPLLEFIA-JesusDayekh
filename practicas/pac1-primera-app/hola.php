e84c630293b3a88f1132911420bc9d0c44456f2b
sebastiandayekh04 <156692475+sebastiandayekh04@users.noreply.github.com>
2024-10-02 15:31:02 +0000

    .

--- practicas/pac1-primera-app/hola.php
+++ practicas/pac1-primera-app/hola.php
@@ -12,15 +12,24 @@ function sayHello ($name) {
     <title>Visual Studio Code Remote :: PHP</title>
 </head>
 <body>
+    <header>
+    <img src="/workspaces/code-space-for-php-demo/practicas/pac1-primera-app/img/logo-fpllefia.jfif" alt="">
+
+    </header>
+
+    <main>
     <?php
     sayHello ("remote world");
     phpinfo();
     ?>
-</body>
-<footer>
+ <div><p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim excepturi, aspernatur ipsum possimus ratione, magnam consequatur dolorum sint illum consectetur odit molestiae tempora nam dicta, quo dolore ullam rem eos.</p></div>
+    </main>
+
+  <footer>
     <label> Sebastian Jesus Dayekh Martinez Numero 5</label>
     <?php
     echo "la fecha es : ".date("Y-M-d");
 ?>
-</footer>
+</footer>  
+</body>
 </html>
