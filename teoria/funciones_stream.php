e47dd71bb491e7935f140c9a5b3f73641f50fc17
sebastiandayekh04 <156692475+sebastiandayekh04@users.noreply.github.com>
2024-10-16 14:34:20 +0000

    16/10/24

--- teoria/funciones_stream.php
+++ teoria/funciones_stream.php
@@ -24,6 +24,26 @@ echo trim($cadena2);
 //9. substr()
 //obtiene una parte de una cadena
 echo substr($cadena2, 4, 4);
-//10.implode
-echo implode($cadena2);
+//10 implode 
+$array = ["hola", "mundo", "php"];
+ echo implode ("", $array);
+ // 12. in_array() mira si existe o no en un array
+ $os = ["Mac", "NT", "Irix", "Linux"]; // Corregido: se usa = en lugar de espacio
+ if (in_array("Irix", $os)) { // Corregido: añadido 'if'
+     echo "Existe Irix";
+ }
+ // 13. array_search
+ // busca un valor en un array y devuelve la clave correspondiente, si no está devuelve false
+ $array = ["manzana", "pera", "naranja"]; 
+ $clave = array_search("naranja", $array); 
+ echo $clave; 
+ 
+ // 14. array_map
+ $arraymap = [1, 2, 3, 4]; 
+ $resultado = array_map(function($numero) { 
+     return $numero * 2; 
+ }, $arraymap); 
+ 
+ print_r($resultado); // Resultado: Array ([0] => 2 [1] => 4 [2] => 6 [3] => 8)
+ //15. 
 ?>
