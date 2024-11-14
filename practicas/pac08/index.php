bc9f8800ac84415f898a5dd344312f12e11d4dea
sebastiandayekh04 <156692475+sebastiandayekh04@users.noreply.github.com>
2024-11-14 17:30:54 +0000

    ...

--- practicas/pac08/index.php
+++ practicas/pac08/index.php
@@ -6,24 +6,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Inici</title>
 </head>
-<body class="d-flex justify-content-center align-items-center vh-100" style="background-image: url('https://basementescaperoom.com/los-angeles/template/images/room-header-bg-thebasement.jpg'); background-size:cover; background-repeat: no-repeat;">
+<body class="d-flex justify-content-center align-items-center vh-100" style="background-image: url('https://i.pinimg.com/originals/98/34/66/983466ddf30a88bf189b1efa13c335f7.jpg'); background-size:cover; background-repeat: no-repeat;">
     <div class="card p-4 bg-dark text-white" style="width: 22rem;">
-        <h2 class="card-title text-center">Benvingut!</h2>
+        <h2 class="card-title text-center">Bienvenido!</h2>
         <form>
             <div class="mb-3">
-                <label for="username" class="form-label">Nom:</label>
+                <label for="username" class="form-label">Nombre:</label>
                 <input type="text" name="username" id="username" class="form-control" required>
             </div>
             <div class="mb-3">
-                <label for="dificultat" class="form-label">Nivell de Dificultat:</label>
-                <select name="" id="dificultat" class="form-select" required>
-                    <option value="">Selecciona un nivell</option>
-                    <option value="facil">Fàcil</option>
-                    <option value="mig">Mig</option>
+                <label for="dificultat" class="form-label">Nivel de Dificultad:</label>
+                <select name="dificultad" id="dificultat" class="form-select" required>
+                    <option value="">Selecciona un nivel</option>
+                    <option value="facil">Facil</option>
+                    <option value="mig">Medio</option>
                     <option value="dificil">Difícil</option>
                 </select>
             </div>
-            <button type="submit" class="btn btn-primary w-100">Comença el Joc</button>
+            <button type="submit" class="btn btn-primary w-100">Comienza el juego</button>
         </form>
     </div>
 </body>
--- practicas/pac08/room3.php
+++ practicas/pac08/room3.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Habitación 5</title>
+    <title>Habitación 3</title>
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="card p-4" style="width: 22rem;">
-        <h2 class="card-title text-center">Habitación 5</h2>
+        <h2 class="card-title text-center">Habitación 3</h2>
         <p class="card-text">Endevinalla: Tengo ciudades, pero no casas. Tengo montañas, pero no árboles. Tengo agua, pero no peces. ¿Qué soy?</p>
         <form method="POST">
             <div class="mb-3">
