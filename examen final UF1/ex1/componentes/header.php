5bc1ecc8849456aedfaaea95f3e8cf8f9b0dde94
sebastiandayekh04 <156692475+sebastiandayekh04@users.noreply.github.com>
2024-11-28 17:58:13 +0000

    ...

--- examen final UF1/ex1/componentes/header.php
+++ examen final UF1/ex1/componentes/header.php
@@ -6,14 +6,19 @@ if(!isset($_SESSION)) {
 ?>
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
   <div class="collapse navbar-collapse" >
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a href="contacto.php"></a>
+      <li class="nav-item">
+        <a class="nav-link" href="/examen final UF1/ex1/componentes/contacto.php">Contacto</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/examen final UF1/ex1/componentes/contacto.php">contacto</a>
+        <a class="nav-link" href="/examen final UF1/ex1/componentes/inicio.php">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/examen final UF1/ex1/componentes/productos.php">Productos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/examen final UF1/ex1/index.php">Index</a>
       </li>
     </ul>
   </div>
