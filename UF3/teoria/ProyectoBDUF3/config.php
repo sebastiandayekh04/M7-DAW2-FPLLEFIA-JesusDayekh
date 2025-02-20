ce8fdb6c474e826714fd792e0b1edbe78736dd3b
sebastiandayekh04 <156692475+sebastiandayekh04@users.noreply.github.com>
2025-02-20 17:16:20 +0000

    docker uptade and config bd fixing bug

--- UF3/teoria/ProyectoBDUF3/config.php
+++ UF3/teoria/ProyectoBDUF3/config.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
-$host = 'mysql-dayekhjesus.alwaysdata.net';
-$dbname = 'dayekhjesus';
-$username = '397997';
-$password = 'sd01102004';
-
-
+// $host = "mysql-sebastiandayekh04.alwaysdata.net";
+// $dbname = "sebastiandayekh04_uf3daw";
+// $username = "400660";
+// $password = "sd01102004";
 
 
-$mysqli = new mysqli($host, $dbname, $username, $password  );
+
+
+$mysqli = new mysqli('mysql-sebastiandayekh04.alwaysdata.net', 'sebastiandayekh04_uf3daw', '400660', 'sd01102004');
+var_dump($mysqli);
 
 if ($mysqli->connect_error) {
-    die ("Error de conexion : " . $mysqli->connect_error);
+    die("Error de conexion : " . $mysqli->connect_error);
 } else {
     echo 'conexion exitosa';
 }
@@ -20,12 +21,15 @@ if ($mysqli->connect_error) {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prueba</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
--- UF3/teoria/ProyectoBDUF3/index.php
+++ UF3/teoria/ProyectoBDUF3/index.php
@@ -1,17 +1,14 @@
 <?php
 require_once 'config.php';
 
-//Fetch all clients
-$result = $mysqli->query("SELECT * FROM clientes ORDER BY id DESC");
-print_r($result);
+// //Fetch all clients
+// $result = $mysqli->query("SELECT * FROM clientes ORDER BY id DESC");
+// print_r($result);
 
 
-$clientes = $result->fetch_all("MYSQLI_ASSOC");
+// $clientes = $result->fetch_all("MYSQLI_ASSOC");
 
-
-print '<br><br><br><br><br><br><br>';
-echo '<pre>';
-print_r($clientes);
-echo '<pre>';
-
-?>
+// print '<br><br><br><br><br><br><br>';
+// echo '<pre>';
+// print_r($clientes);
+// echo '<pre>';
